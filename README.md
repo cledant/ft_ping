70e10b66d098164de44535b4d62a1f4f16c279e2
cledant <cledant@student.42.fr>
2019-06-10 10:30:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ft_ping
+
+This 42 project aims to reimplement ping binary.
+
+## Compiling
+
+## Usage
---
+++ srcs/ft_ping.c
@@ -0,0 +1,10 @@
+#include "ft_ping.h"
+
+int
+main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+    puts("Nyaaa");
+    return (0);
+}
